604fed3180790bd7b5fa00dcbc7984fff907f6a1
Jamin Son <sonjamin98@gmail.com>
2021-03-23 04:17:30 +0000

    Removed Unnecessary Files

--- main.py
+++
@@ -1,9 +0,0 @@
-from EDA_Intial_Preprocessing import preprocessing
-import pandas as pd
-
-df = pd.read_csv('data.csv')
-
-# Returns lists of datasets that have been processed
-# For each list, 0: x training data, 1: x testing data, 2: y training data and 3: y testing data
-data_preprocess = preprocessing.Preprocessing(df)
-standardised_data, normalized_data, unit_normalized_data = data_preprocess.preprocess()
